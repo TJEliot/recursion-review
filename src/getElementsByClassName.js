5ea52c853aada5d0386ae728a006c8d666c24760
bgaesop <bgaesop@gmail.com>
2017-11-27 12:37:10 -0800

    First pair programming commit for recursion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,27 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+//input object
+//output array of subElements 
+var getElementsByClassName = function(className, doc) {
+  if(doc === undefined){
+    doc = document.body; 
+  }
+  var returner = []; 
+  if (doc.className !== undefined){
+    var ourClasses = doc.className.split(" ");
+  } else {
+    var ourClasses = [];
+  }
+  if (ourClasses.includes(className)){
+    returner.push(doc);
+  }
+  if (doc.childNodes.length !== 0){
+    for (var i=0; i<doc.childNodes.length; i++){
+      returner = returner.concat(getElementsByClassName(className, doc.childNodes[i]));
+    }
+  }
+  return returner;
 };
+
+//call it like myObject.getElementsByClassName("myClass");
