ba298fb848a0211b29339fea1f1f147ed0af3660
bgaesop <bgaesop@gmail.com>
2017-11-27 16:44:46 -0800

    working on parseJSON

--- spec/parseJSONSpec.js
+++ spec/parseJSONSpec.js
@@ -8,7 +8,13 @@ describe('parseJSON', function() {
       var equality = _.isEqual(result, expected); // why can't we use `===` here?
       // Replace this line with an `expect` statement that tests
       // the behavior described by the `it` string
-      throw new Error('Test is missing.')
+      //throw new Error('Test is missing.')
+      if (equality) {
+        console.log('passed');
+      } else {
+        console.log('we done messed up, expected ' + JSON.stringify(expected) + 'but actually got ' + JSON.stringify(result));
+      }
+      expect(equality).to.equal(true);
     });
   });
 
@@ -18,7 +24,7 @@ describe('parseJSON', function() {
         parseJSON(test);
       };
       // if you'd prefer, you can write your version of parseJSON 
-      // so that it passes this test instead of the one on line 21. 
+      // so that it passes this test instead of the one on line 23. 
       // expect(parseJSON(test)).to.equal(undefined);
       expect(fn).to.throw(SyntaxError);
     });
--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,44 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  if (json === '[]') {
+    return [];
+  }
+  if (json[0] === '[' && json[json.length - 1 ] === ']') {
+    var returner = [];
+    returner = returner.concat(json.split(','));
+  }
+  if (json === '{}') {
+    return {};
+  }
+  if (json[0] === '{' && json[json.length - 1 ] === '}') {
+    var returner = {};
+    // holder array that is json split on :
+    // example {"one": 1, "two": 2};
+    // becomes ["one", "1, 'two'", "2"];
+    // then split each element on ,
+    // becomes ["one", "1", "two", "2"];
+    var holder1 = json.split(':');
+    console.log('holder1 is ' + holder1);
+    var holder2 = [];
+    if (json.length > 2) {
+      for (var i = 0; i < holder1.length; i++) {
+        var myElem = holder1[i].split(',');
+        // for (var j = 0; j < myElem.length; j++) {
+        //   myElem[j] = myElem[j].slice(2, myElem[j].length - 2);
+        // }
+        console.log('holder2 is in the loop and is ' + holder2);
+      }
+      //holder2[0] = holder2[0].slice(1, holder2[0].length -1);
+      console.log('holder2 is ' + holder2);
+      for (var j = 0; j < holder2.length; j += 2) {
+        returner[holder2[j]] = holder2[j + 1];
+        console.log('returner is ' + JSON.stringify(returner));
+        // set the key "one" equal to the value "1" in returner
+      }
+      //alert(returner); 
+    }
+    returner = holder2;
+  }
+  return returner;
 };
